a919db04e4c6f1c9532ec84564f7c67142866d31
Bruno Crivelaro <brunosantoscrivelaro@gmail.com>
2020-10-31 13:11:28 -0300

    refactor WeatherTweeterService

--- app/services/weather_tweetter_service.rb
+++ app/services/weather_tweetter_service.rb
@@ -48,11 +48,14 @@ class WeatherTweetterService
   end
 
   def twitter_client
-    @twitter_client ||= Twitter::REST::Client.new do |config|
-      config.consumer_key        = ENV['TWITTER_CONSUMER_KEY']
-      config.consumer_secret     = ENV['TWITTER_CONSUMER_SECRET']
-      config.access_token        = ENV['TWITTER_ACCESS_TOKEN']
-      config.access_token_secret = ENV['TWITTER_ACCESS_SECRET']
-    end
+    return @twitter_client if @twitter_client
+
+    config = {
+      consumer_key: ENV['TWITTER_CONSUMER_KEY'],
+      consumer_secret: ENV['TWITTER_CONSUMER_SECRET'],
+      access_token: ENV['TWITTER_ACCESS_TOKEN'],
+      access_token_secret: ENV['TWITTER_ACCESS_SECRET']
+    }
+    @twitter_client ||= Twitter::REST::Client.new(config)
   end
 end
